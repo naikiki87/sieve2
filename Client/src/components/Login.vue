4c9c6ef543ef421e6458d9de7808de9d129055f2
‍김수영(대학원학생/일반대학원 컴퓨터과학과) <naikiki87@naver.com>
2020-08-25 13:56:21 +0900

    m

--- Client/src/components/Login.vue
+++ Client/src/components/Login.vue
@@ -65,10 +65,12 @@ export default {
       var success = (await axios.post(api, params)).data.success
       console.log("suc : ", success)
       if(success) {
-        location.href = "http://localhost:8080/main"
+        // location.href = "http://localhost:8080/main"
+        location.href = "http://165.132.105.40:50000/main"
       }
       else {
-        location.href = "http://localhost:8080"
+        // location.href = "http://localhost:8080"
+        location.href = "http://165.132.105.40:50000"
       }
 
     },
